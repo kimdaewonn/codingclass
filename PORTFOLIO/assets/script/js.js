22d102bc597da762a69a4939f10f2a4f4dcb8352
kimdaewonn <eodnjs9605@gmail.com>
2022-12-06 00:47:10 +0900

    1206

--- PORTFOLIO/assets/script/js.js
+++ PORTFOLIO/assets/script/js.js
@@ -1,5 +1,7 @@
+// 데이터 애니에미션
 const s = skrollr.init();
 
+// 스크롤탑
 function scroll() {
     let scrollTop = window.screenY || document.documentElement.scrollTop;
     document.querySelector(".scrollTop").innerText = Math.ceil(scrollTop);
